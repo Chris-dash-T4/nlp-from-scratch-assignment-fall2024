fe937863dea741359ebab39a368d8b8a98be5207
Ishaan Harry Kavoori <ishaanharry@live.com>
2024-10-18 19:25:28 -0400

    more rag boilerplating

--- rag.py
+++ rag.py
@@ -8,18 +8,23 @@ from haystack.components.builders import PromptBuilder
 import os
 from getpass import getpass
 from haystack.components.generators import OpenAIGenerator
+from haystack import Pipeline
 
+# document store integrations in case we need to scale up https://haystack.deepset.ai/integrations?type=Document+Store
 document_store = InMemoryDocumentStore()
 
+# must replace with our dataset
 dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
 docs = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
 
+# options for different embedders https://docs.haystack.deepset.ai/docs/embedders
 doc_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
 doc_embedder.warm_up()
 
 docs_with_embeddings = doc_embedder.run(docs)
 document_store.write_documents(docs_with_embeddings["documents"])
 
+# model depends on embedding model used earlier
 text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
 
 retriever = InMemoryEmbeddingRetriever(document_store)
@@ -38,6 +43,24 @@ Answer:
 
 prompt_builder = PromptBuilder(template=template)
 
+# change this for whatever our llama api key is
 if "OPENAI_API_KEY" not in os.environ:
     os.environ["OPENAI_API_KEY"] = getpass("Enter OpenAI API key:")
 generator = OpenAIGenerator(model="gpt-4o-mini")
+
+
+basic_rag_pipeline = Pipeline()
+# Add components to your pipeline
+basic_rag_pipeline.add_component("text_embedder", text_embedder)
+basic_rag_pipeline.add_component("retriever", retriever)
+basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
+basic_rag_pipeline.add_component("llm", generator)
+
+# Now, connect the components to each other
+basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
+basic_rag_pipeline.connect("prompt_builder", "llm")
+
+question = "What does Rhodes Statue look like?"
+response = basic_rag_pipeline.run({"text_embedder": {"text": question}, "prompt_builder": {"question": question}})
+print(response["llm"]["replies"][0])
